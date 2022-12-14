a7aa8cc5404e03e63685588198b19e844a402619
Manabu Kikuchi <16217397+kerobot@users.noreply.github.com>
2022-12-14 15:57:38 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import "./styles.css";
+
+export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClockUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClockUser1}>id=1のuser</button>
+    </div>
+  );
+}
